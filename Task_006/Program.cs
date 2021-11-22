be92b70036cf31f0ce5fc97513c32baac7a2c4eb
Маис <Kozannostra777@mail.ru>
2021-11-22 21:17:46 +0300

    закомитили Task_006

---
+++ Task_006/Program.cs
@@ -0,0 +1,12 @@
+﻿double numberA = new Random().Next(1, 30); // 1 2 3 4 ... 30
+
+if (numberA %2== 0)
+{
+    Console.Write(numberA);
+    Console.WriteLine(" Четное число");
+}
+else
+{
+    Console.Write(numberA);
+    Console.WriteLine(" Нечетное");
+}
