fc45bb01374a53a54e32af5c7e5382e963af4516
Маис <Kozannostra777@mail.ru>
2021-11-22 21:17:34 +0300

    закомитили Task_002

---
+++ Task_002/Program.cs
@@ -0,0 +1,10 @@
+﻿double numberA = 105;
+double numberB = 10;
+
+if((numberB * numberB) == (numberA)) 
+{
+    Console.WriteLine("условие выполнено");
+}
+else {
+    Console.WriteLine("Условие не выполнено");
+}
