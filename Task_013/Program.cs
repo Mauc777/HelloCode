5025c396de6dc984d1b5624b4bc08889b037e0bc
Маис <Kozannostra777@mail.ru>
2021-11-22 21:18:18 +0300

    закомитили Task_013

---
+++ Task_013/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = 10;
+int numberB = 3;
+double result = (numberA/numberB);
+
+if(numberA%2==0)System.Console.WriteLine("{0} - кратно 2.",numberB);
+
+Console.WriteLine(numberA/numberB);
+{
+    Console.WriteLine("result");    
+};
