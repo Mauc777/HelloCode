5d6ed5318ff7f49810a136fb9586c884fb9e6066
Маис <Kozannostra777@mail.ru>
2021-11-25 11:06:46 +0300

    add Task_003

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -1,2 +1,5 @@
-﻿int numA = 25, numB = 5;
-Console.WriteLine(numA * numB);
+﻿Console.Clear();
+string[] Weeks = {" ","Mon","Thu","Wen","Thr","Fra","Sat","sun"};
+Console.Write("Введите номер дня недели: ");
+int index = int.Parse(Console.ReadLine());
+Console.WriteLine(Weeks[index]);
