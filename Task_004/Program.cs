ec4f8d64f11a681377b09dffdf9f01a4d9a6bc1b
Маис <Kozannostra777@mail.ru>
2021-11-26 19:44:36 +0300

    Коммит Task_4

---
+++ Task_004/Program.cs
@@ -0,0 +1,17 @@
+﻿int NumA = 13; 
+int NumB = 6; 
+int NumC = 9; 
+ 
+int max = NumA; 
+ 
+if (NumB > max)  
+{ 
+    max = NumB; 
+} 
+ 
+if (NumC > max)  
+{ 
+    max = NumC; 
+} 
+ 
+Console.WriteLine(max);
