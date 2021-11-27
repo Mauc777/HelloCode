25fedb6a20feecd54f2378fa003fcad07db7f253
Маис <Kozannostra777@mail.ru>
2021-11-27 22:09:59 +0300

    add file Method_4 с разбором 4-ого Метода(она же Функция)

---
+++ Trening_Test_Lesson_Method/Method_4/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+string Method4(int count, string text)
+{
+    int i =0;
+    //string result = "";   //""; = такое значением обозначается значение пустая строка.
+    string result = String.Empty; // = также обозначает пустая строка, но оно и из перевода понятно
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+
+}
+string res = Method4(15, "Gamburg_show");
+Console.WriteLine(res);
