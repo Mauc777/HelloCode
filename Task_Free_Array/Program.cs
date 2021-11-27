bc0c7655ad6bd6211e550bd527401c8f7c5bbc7f
Маис <Kozannostra777@mail.ru>
2021-11-27 11:33:55 +0300

    промежуточный коммит4

---
+++ Task_Free_Array/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;    
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+
+// делал пошаглово разбираясь в "слоях и блоках" кода раздельно, сделал вывод 
+// ..что  первый void не работает без второго void-a!!
+// и также наоборот, то есть нам нужно задать в функциях что мы хотим потом их вызывая поименно 
+// ..получать нужные значения!!  !!wq!!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
