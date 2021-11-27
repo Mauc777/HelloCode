dff97c9d98ca1ef6b2389b2f66087c1711a5e259
Маис <Kozannostra777@mail.ru>
2021-11-27 21:00:24 +0300

    add Method2 &Method21  закомитили 2ой вид Медода..

---
+++ Trening_Test_Lesson_Method/Method_2_void/Program.cs
@@ -0,0 +1,25 @@
+﻿
+/// void Methode #2
+
+void Methode2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Methode2("Данный пробный текст, привет Вацок!");
+///Убрать слеш для вызова нижнего Метода!
+//Methode2(msg: "Данный пробный текст, привет Вацок!");  //так делают если Аргументов НЕСКОЛЬКО!!!
+
+// НАпример когда Аргументов НЕСКОЛЬКО!
+
+void Method21(string msg , int count)
+{int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        //count++; не верно, ножно увеличивать i !!!
+        i++;
+    }
+}
+//Method21("Привет, Вацок", 4); будет правильнне на ниж.строке!
+Method21(msg: "Привет, Вацок", count: 10);
+Method21(count: 10, msg: "Привет, Вацок");
