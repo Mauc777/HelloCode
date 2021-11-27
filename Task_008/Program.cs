7874ecf3ddb4a6f238ddd53412ec6694e7ca7944
Маис <Kozannostra777@mail.ru>
2021-11-27 11:33:32 +0300

    промежуточный коммит3

---
+++ Task_008/Program.cs
@@ -0,0 +1,35 @@
+﻿//Показать четные числа от 1 до N
+
+Console.Clear();
+int[] num = {1, 200};
+void FillArray(int[] collect);
+
+{
+    int lenght = num.Length;
+    int index = 0;
+    while (index < lenght)
+   
+    {
+        if(num[index]%2 == 0)
+        {
+            FillArray(collect);
+        }
+        
+        else
+            {
+            num[index]%2 ! = 0;
+            Console.WriteLine();
+            }
+     index++;
+    }        
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();      
+
+
+
+
+
+
+
