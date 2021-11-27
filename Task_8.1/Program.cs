8eedd1c389d4926ac01ca7e18d95bf9c321c23fb
Маис <Kozannostra777@mail.ru>
2021-11-27 11:34:17 +0300

    промежуточный коммит5

---
+++ Task_8.1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Показать четные числа от 1 до N
+Console.Clear(); 
+int[] mass = {1,2,3,4,5,6,7,8,9};
+int size = 8;
+int index = 0;
+
+while(index < size)
+    {
+        if (mass[index] % 2!=0)
+        {
+            Console.WriteLine("Это число ЧЕТНОЕ");
+        }
+        index++;
+    }        
+      
+    
+Console.WriteLine("Конец");
+
+
+
+
+
+
+
