6b4c94bd1c6309a82b71fdf8bf52c7981983cb20
Маис <Kozannostra777@mail.ru>
2021-11-27 11:32:45 +0300

    промежуточный коммит2

---
+++ Task_005/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//также работает на рандомных числах
+//
+// функция y=f(a) 
+ 
+//Console.Clear(); 
+ 
+//int number = new Random().Next(1, 20); 
+//double f(double a) 
+//{ 
+//    return (a*a+1); 
+//} 
+//Console.WriteLine(number); 
+//Console.WriteLine(f(number));
+
+
+
+// функция y=f(a) 
+//y=(a*a+1);
+Console.Clear();
+double f(double a)
+{ 
+    return  (a*a+1);
+}
+Console.WriteLine(f(15));
