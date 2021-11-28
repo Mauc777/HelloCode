373b51f7012983abbb549fb0d292cc76bfdd027d
Маис <Kozannostra777@mail.ru>
2021-11-28 17:16:40 +0300

    Новый цикл for - его можно использовать вместо while+ цикл можно использовать в цикле многократно!!

---
+++ Trening_Test_Lesson_Method/New_Method_nameFOR/Program.cs
@@ -0,0 +1,16 @@
+﻿string Method4(int count, string text)
+{
+    //int i =0; это уже есть в for
+    //string result = "";   //""; = такое значением обозначается значение пустая строка.
+    string result = String.Empty; // = также обозначает пустая строка, но оно и из перевода понятно
+    for (int i = 0; i < count; i++)
+    // while (i < count)  это тоже содержиться уже в for
+    {
+        result = result + text;
+        // i++;  это уже есть в for
+    }
+    return result;
+
+}
+string res = Method4(15, "Gamburg_show");
+Console.WriteLine(res);
