2945dca4ef3a0ef5d8965304e8e1b12c0d5adb53
Маис <Kozannostra777@mail.ru>
2021-11-29 16:53:38 +0300

    длбавленн комментарий в Task_7

---
+++ Task_7/Program.cs
@@ -0,0 +1,12 @@
+﻿//Показать числа от -N до N
+
+Console.WriteLine("Enter num");
+int FirstNum = int.Parse(Console.ReadLine());
+int NextNum = -FirstNum;
+    while (NextNum <= FirstNum)
+    {
+       Console.WriteLine(NextNum);
+       NextNum++; 
+    }
+
+// задача решена совмиестнвми усилиями на семинаре
