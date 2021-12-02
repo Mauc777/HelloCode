027a8de929a8319c95fa1a0674a5f347e3b9126d
Маис <Kozannostra777@mail.ru>
2021-12-02 10:05:38 +0300

    Добавлено ЛИЧНОЕ РЕШЕНИЕ задачи №5/ Task_015

---
+++ Task_015/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Дано число. Проверить кратно ли оно 7 и 23
+
+Console.Clear();
+//NumA это число для вычисления кратности
+Console.WriteLine("Enter numA ");
+double A = double.Parse(Console.ReadLine());
+
+double B = 7;
+double C = 23;
+
+while (A % B==0)
+{
+   Console.WriteLine("число кратно {B} ");
+   break;    
+} 
+
+if (A % C ==0)
+{
+   Console.WriteLine("Число кратно {C} ");
+   //break;
+}
+
+
+
+
