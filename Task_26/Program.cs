79f0ad3e6fd6b336e5f0838ba38c46421a00df1c
Маис <Kozannostra777@mail.ru>
2021-12-02 10:10:10 +0300

    Task_26++

---
+++ Task_26/Program.cs
@@ -0,0 +1,20 @@
+﻿//Возведите число А в натуральную степень B используя цикл
+
+Console.Clear();
+Console.WriteLine("Enter numA ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter numB ");
+int B = int.Parse(Console.ReadLine());
+int result = 1;
+
+for (int i = 0; i < B; i++)
+{
+    result =  result * A ;
+}
+
+
+ Console.WriteLine(B);
+    Console.WriteLine(result);
+
+
+
