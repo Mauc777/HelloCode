1fc507966de29a229495c18aa2d4c4cad7687590
Маис <Kozannostra777@mail.ru>
2021-12-02 11:02:29 +0300

    add Task_014

---
+++ Task_014/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+void Find3Num(string NumberA)
+
+{if (NumberA.Length > 2) Console.WriteLine($"Третья цифра числа: {NumberA[2]}");
+else Console.WriteLine("Нет третьей цифры");}
+
+Console.Write("Введите любое число: ");
+//string NumberA = Console.ReadLine();
+
+Find3Num(Console.ReadLine());
