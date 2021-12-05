8ed8b37c33c736833e2b6815a15d7165176645bb
Маис <Kozannostra777@mail.ru>
2021-12-05 18:54:59 +0300

    Добавил решение задачи №11! Решил ее за 2 дня (мелкими перебежками) так как было немного времени!!

---
+++ Task_011/Program.cs
@@ -0,0 +1,43 @@
+﻿//11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.Clear();
+Console.WriteLine("Введите заданное число от 10 до 99");
+int numberA =int.Parse(Console.ReadLine());
+//Console.WriteLine(numberA);
+
+int count = 1;
+int result1 = 0; 
+int result2 = 0;
+int numberB = numberA;
+
+    for (int i = 0; i < 1; i++)  
+    {
+        result1 = result1 + numberA/10;  //нахождение целого числа
+        Console.WriteLine($"целое число от деления = {result1} ");
+    }    
+    for (int j = 0; j < 1; j++)
+    {
+        result2 = result2 + numberB%10;  //нахождение остатка
+        Console.WriteLine();
+        Console.WriteLine($"остаток от деления на 10 равен = {result2} ");
+    }
+        if (result1 > result2)  
+        {
+        Console.WriteLine();
+        Console.WriteLine($"наибольшей цифрой числа является 1-ое число = {result1} ");
+        }
+        else 
+        {            
+        Console.WriteLine($"наибольшей цифрой числа является 2-ое число = {result2}");
+        } 
+
+
+
+
+    
+       
+    
+   
+    
+  
+    
