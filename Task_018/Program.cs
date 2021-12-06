91c9a727878b348e9766942a7bb31ebe88bc8a6a
Маис <Kozannostra777@mail.ru>
2021-12-06 13:55:59 +0300

    Добавлена задача 18(Task_018

---
+++ Task_018/Program.cs
@@ -0,0 +1,49 @@
+﻿//18 Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+Console.Clear();
+bool x, y;
+x = true;
+y = true;
+
+bool result = true;
+
+
+if (!(x || y) == (!x && !y) )
+{
+    Console.WriteLine(true);    
+
+} 
+
+else result = false;
+
+
+x = true;
+y = false;
+
+if (!(x|| y) == (!x && !y) )
+{
+    Console.WriteLine(true);    
+} 
+else result = false;
+
+x = false;
+y = true;
+if (!(x|| y) == (!x && !y) )
+{
+    Console.WriteLine(true);    
+} 
+else result = false;
+
+x = false;
+y = false;
+if (!(x|| y) == (!x && !y) )
+{
+    Console.WriteLine(true);    
+} 
+else result = false;
+
+if (result)
+Console.WriteLine($"Утверждение Истинно"  );
+else
+{
+    Console.WriteLine($"Утверждение НЕ Истинно"  );    
+}
