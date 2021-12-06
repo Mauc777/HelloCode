efbce4949b93dfccc900d955c8a3e281be4162d8
Маис <Kozannostra777@mail.ru>
2021-12-06 14:04:29 +0300

    Добавлена задача 21(Task_21)!!!

---
+++ Task_21/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+Console.Clear();
+Console.Write("Enter Num: ");
+string x= Console.ReadLine();
+
+     if (x[0] == x[4]  &&  x[1] == x[3])
+     {
+        Console.WriteLine("Палиндром"); 
+     }
+     else
+     {
+           Console.Write("Не палиндром");
+     }
+
