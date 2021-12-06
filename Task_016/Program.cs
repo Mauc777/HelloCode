7d4c78447281a094029f72b3593e53371988da4b
Маис <Kozannostra777@mail.ru>
2021-12-06 11:01:21 +0300

    Add Task_016

---
+++ Task_016/Program.cs
@@ -0,0 +1,11 @@
+﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Clear();
+string[] Weeks = {"  ", "mon", "thu", "wen", "thr", "fra", "sat", "sun"};
+Console.WriteLine("Enter number:  ");
+int index = int.Parse(Console.ReadLine());
+Console.Write(Weeks[index]);
+if (index == 6 || index == 7 )
+{
+    Console.Write(" Выходной");
+}
+
