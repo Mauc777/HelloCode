32b0386234db2294e0e3a3741757217a6c1441c8
Маис <Kozannostra777@mail.ru>
2021-12-06 14:03:23 +0300

    Добавлена задача 12(Task_12)!

---
+++ Task_12/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 12. Удалить вторую цифру трёхзначного числа
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int A = int.Parse(Console.ReadLine());
+int B = A/100;
+int C = (A % 100) % 10;
+Console.Write(B);
+Console.WriteLine(C);
