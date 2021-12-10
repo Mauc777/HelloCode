ec93aa3a52981722a7027874db4c7a412eb3ab26
Маис <Kozannostra777@mail.ru>
2021-12-10 18:35:57 +0300

    добавлено решение 13 задачи

--- Task_12/Program.cs
+++ Task_12/Program.cs
@@ -3,7 +3,7 @@
 Console.Clear();
 Console.WriteLine("Введите трехзначное число: ");
 int A = int.Parse(Console.ReadLine());
-int B = A/100;
+int B = A / 100;
 int C = (A % 100) % 10;
 Console.Write(B);
 Console.WriteLine(C);
