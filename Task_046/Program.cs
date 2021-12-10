f7eaab9994542f174d9d7506a8b36edb1cb74f05
Маис <Kozannostra777@mail.ru>
2021-12-10 18:30:05 +0300

    некоторые исправдения-поработать над отображением кода

--- Task_046/Program.cs
+++ Task_046/Program.cs
@@ -13,25 +13,26 @@ double k = 2;
 char[] separators = new char[] { ' ', ',', '(', ')' };
 string[] Nums = (s.Split(separators, StringSplitOptions.RemoveEmptyEntries));
 
-int[] X = new int[(Nums.Length)/2];
-int[] Y = new int[(Nums.Length)/2];
+int[] X = new int[(Nums.Length) / 2];
+int[] Y = new int[(Nums.Length) / 2];
 
 for (int i = 0; i < Nums.Length; i++)
 {
     int Index = 0;
-    
-    if (i%2==0)
+
+    if (i % 2 == 0)
     {
         X[Index] = (Convert.ToInt32(Nums[i]));
-    
-        Console.Write($"({X[Index]*k},");
-    
+
+        Console.Write($"({X[Index] * k},");
+
     }
-    else 
+    else
     {
-        Y[IndexY] = (Convert.ToInt32(Nums[i]));
-        
-        Console.Write($" {Y[Index]*k}) ");
-       
+        Y[Index] = (Convert.ToInt32(Nums[i]));
+
+        Console.Write($" {Y[Index] * k}) ");
+
         // if (X[
-}
+    };
+}    
