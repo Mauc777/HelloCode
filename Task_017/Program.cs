8ce52ec96d118f01fd17329a1b1f4ddad1f42fbd
Маис <Kozannostra777@mail.ru>
2021-12-17 12:46:43 +0300

    Добавлено решение (мое) Task_017 (Задачи №17)

--- Task_017/Program.cs
+++ Task_017/Program.cs
@@ -1,35 +1,19 @@
 ﻿//17 По двум заданным числам проверять является ли одно квадратом другого
-
-// TODO написать медот для проверки на квадрат 
 Console.Clear();
-// int NumberA = 12;
-// int NumberB = 144;
-//int result = 0;
-//int NumberA, int NumberB
+int NumberA = 12;
+int NumberB = 144;
+int result = 0;
 
-int DedTop(int result = 0, int NumberA = 12, int NumberB = 144)
+result = result + (int)Math.Pow(NumberA, 2);
+
+if (result == NumberB)
 {
-    //int result = 0;
-    result = (int)Math.Pow(NumberA, 2);
-    result = result + (int)Math.Pow(NumberA, 2);
-    if (result == NumberB)
-    {
-        //Console.Write(result);
-        Console.Write($"Данное число {NumberA} является квадратом {NumberB}");
-
-    }
-
-
-    else
-    {
-        Console.Write($"Данное число {NumberA} НЕ является квадратом {NumberB}");
-    }
-    return result;
-
+    Console.Write($"Данное число {NumberA} является квадратом {NumberB}");
+}
+else
+{
+    Console.Write($"Данное число {NumberA} НЕ является квадратом {NumberB}");
 }
 
 
-
-
-Console.WriteLine(result);
 
