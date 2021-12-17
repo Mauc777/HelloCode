e5d62b31ad968602e4415de1c1225537409d6661
Маис <Kozannostra777@mail.ru>
2021-12-17 09:32:06 +0300

    Решение 51ой задачи. (Решали ее на семинаре 8 16.12.21г вместе)

---
+++ Task_051/Program.cs
@@ -0,0 +1,15 @@
+﻿//51. Задать двумерный массив следующим правилом: Aₘₙ = m+n
+Console.Clear();
+int m = 110;
+int n = 115;
+int[,] arr = new int[n, m];
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        arr[i,j] = i+j;
+        Console.Write($" {arr[i, j]} ");
+        
+    }
+    Console.WriteLine();
+}
