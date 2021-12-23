2317af6ded0a2613b644e4adcbf6f903eaa96b59
Маис <Kozannostra777@mail.ru>
2021-12-23 11:05:20 +0300

    Решение задачи №68 (Решали на Семинаре 9

---
+++ Task_068/Program.cs
@@ -0,0 +1,8 @@
+﻿//  Показать натуральные числа от M до N, N и M заданы
+void NNumbers(int N, int M)
+{
+    if (M == N) { Console.WriteLine(M); return; }
+    if (M < N) { Console.WriteLine(M); NNumbers(N, M + 1); }
+    else if (M > N) { Console.WriteLine(M); NNumbers(N, M - 1); }
+}
+NNumbers(10, 2);
