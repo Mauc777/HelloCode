5718c572ee29f116096760d8f5cfbd5493250c50
Маис <Kozannostra777@mail.ru>
2021-12-23 11:56:59 +0300

    добавлено решение  Задачи №69 (решали на Семинаре 9)

---
+++ Task_069/Program.cs
@@ -0,0 +1,14 @@
+﻿// 69. Найти сумму элементов от M до N, N и M заданы
+Console.Clear();
+void SumNum(int N, int M, int sum)
+{
+    if (M == N)
+    {
+        sum = sum + M; Console.WriteLine(M); Console.WriteLine($"Сумма элементов от M до N = {sum}");
+        return;
+    }
+    if (M < N) { sum = sum + M; Console.WriteLine(M); SumNum(N, M + 1, sum); }
+    else if (M > N) { sum = sum + M; Console.WriteLine(M); SumNum(N, M - 1, sum); }
+}
+SumNum(10, 2, 0);
+
