b846a20be2d655d3a97f52b3702c2f62296c88ca
Маис <Kozannostra777@mail.ru>
2021-12-27 11:36:39 +0300

    Решение задачи №72 на семинаре 10

---
+++ Task_072/Program.cs
@@ -0,0 +1,15 @@
+﻿// 72. Написать программу возведения числа А в целую стень B
+
+double NumDegree(double a, int b)
+{
+    if (b == 0)
+    {
+        return 1;
+    }
+    if (b < 0)
+    {
+        return NumDegree(a, b + 1) / a;
+    }
+    return NumDegree(a, b - 1) * a;
+}
+Console.WriteLine(NumDegree(2, 3));
