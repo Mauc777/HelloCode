48c09f56c99e1620349c14791a10ca4819bd19f4
Маис <Kozannostra777@mail.ru>
2022-02-03 18:47:02 +0300

    Через цикл For решил и добавил Task_009\Задача 9

---
+++ Task_009/Program.cs
@@ -0,0 +1,19 @@
+﻿//9. Показать последнюю цифру трёхзначного числа
+Console.Clear();
+int[] arrayA = { 158, 585, 685, 784, 108, 955, 16 };
+for (int i = 0; i < arrayA.Length; i++)
+{
+    if (arrayA[i] >= 100)
+    {
+        Console.Write('[');
+        Console.Write(arrayA[i] % 10);
+        Console.Write(']');
+    }
+
+}
+Console.WriteLine("[конец]");
+
+// Console.Clear();
+// var numberA = 156;
+// Console.WriteLine($"{numberA % 10}");
+// Console.WriteLine("Конец бля");
