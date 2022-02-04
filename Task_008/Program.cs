1ab40313be33f4039838611a7e620f7910606ff3
Маис <Kozannostra777@mail.ru>
2022-02-04 21:53:16 +0300

    Добавил решение Задачи 8 ЧЕРЕЗ МЕТОДЫ(работал не один!!!)

--- Task_008/Program.cs
+++ Task_008/Program.cs
@@ -1,31 +1,184 @@
 ﻿//Показать четные числа от 1 до N
 
-Console.Clear();
-int[] num = {1, 200};
-void FillArray(int[] collect);
-
+int[] FillArray()
 {
-    int lenght = num.Length;
-    int index = 0;
-    while (index < lenght)
-   
+    int[] array = new int[10]; // объявл пуст массива с размером 10
+    for (int i = 0; i < array.Length; i++)
     {
-        if(num[index]%2 == 0)
-        {
-            FillArray(collect);
-        }
-        
-        else
-            {
-            num[index]%2 ! = 0;
-            Console.WriteLine();
-            }
-     index++;
-    }        
+        array[i] = new Random().Next(0, 10); //каждому элементу присваем рандом число
+    }
+    
+    return array; // обязательно возвращаем полученный массив??!!!!!!!
+    
 }
-FillArray(array);
-PrintArray(array);
-Console.WriteLine();      
+
+int[] Find(int[] ArrayA)
+{
+    int[] ArrayB = new int[0];
+    int j = 0;
+
+
+    for (int i = 0; i < ArrayA.Length; i++)  //??
+    {
+        if (ArrayA[i] % 2 == 0)
+        {
+            int Length = ArrayB.Length;
+            Array.Resize(ref ArrayB, Length + 1);
+            ArrayB[j] = ArrayA[i];
+
+            j++;
+        }
+    }
+    return (ArrayB);
+}
+
+void PrintArray(int[] ArrayA, int[] ArrayB)
+{
+    Console.Write("[");
+    for (int i = 0; i < ArrayA.Length; i++)
+    {
+        Console.Write(ArrayA[i]);
+        if (i != ArrayA.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("] -> [");
+    for (int i = 0; i < ArrayB.Length; i++)
+    {
+        Console.Write(ArrayB[i]);
+        if (i != ArrayB.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+void ArrayCheck(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+    }
+}
+
+int[] ArrayA = FillArray();
+// int[] ArrayB = Find(ArrayA);
+ArrayCheck(ArrayA);
+// Console.WriteLine();
+// ArrayCheck(ArrayB);
+
+PrintArray(ArrayA, Find(ArrayA));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.Clear();
+// int[] ArrayA = { 12, 13, 18, 20, 28, 36, 88, 90 };
+
+
+// int[] FillArray(int[] ArrayA)
+// {
+//     int[] ArrayB = new int[0];
+//     int j = 0;
+//     int Length = ArrayB.Length;
+
+//     for (int i = 0; i < ArrayB.Length; i++)
+//     {
+//         if (ArrayA[i] % 2 == 0)
+//         {
+//             ArrayA[i] = ArrayB[j];
+//             Length = Length + 1;
+//             j++;
+//         }
+//     }
+//     return (ArrayB);
+// }
+
+// void PrintArray(int[] ArrayA, int[] ArrayB)
+// {
+//     for (int i = 0; i < ArrayA.Length; i++)
+//     {
+//         Console.Write($"{ArrayA[i]} ");
+//         if (i != ArrayA.Length - 1)
+//         {
+//             Console.Write(",");
+//         }
+//     }
+//     Console.WriteLine();
+
+//     for (int i = 0; i < ArrayB.Length; i++)
+//     {
+//         Console.Write($"{ArrayB[i]} ");   
+//         if (i != ArrayB.Length - 1)
+//         {
+//             Console.Write(",");
+//         }
+//     }
+
+// }
+
+// PrintArray(ArrayA, FillArray(ArrayA));
+
+
+
+
+
 
 
 
